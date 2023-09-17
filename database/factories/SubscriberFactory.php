17cfd85143cf6b28fb9c6d6adab8a7ed434f7c73
gabrielestefono <gabrielestefono@hotmail.com>
2023-09-16 21:36:02 -0300

    Setando a Factory

--- database/factories/SubscriberFactory.php
+++ database/factories/SubscriberFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Subscriber>
@@ -17,7 +18,8 @@ class SubscriberFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'email' => fake()->unique()->safeEmail(),
+            'email_verified_at' => now(),
         ];
     }
 }
